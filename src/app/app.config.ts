0128bf2082ed4a410d3a75f4fbb0e331e6326ab4
Ondřej Michejda <ondra.michejda@gmail.com>
2025-02-10 14:53:34 +0100

    ngrx added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,17 +5,20 @@ import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideZoneChangeDetection({eventCoalescing: true}),
-        provideRouter(routes),
-        provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({
-            "projectId": "bjjtournament-4538b",
-            "appId": "1:1012794337509:web:8cf044ab0853b812460c0c",
-            "storageBucket": "bjjtournament-4538b.firebasestorage.app",
-            "apiKey": "AIzaSyBLTBSPgxF_PWyzQpIOGb5Uu9Ggs-IO3Ss",
-            "authDomain": "bjjtournament-4538b.firebaseapp.com",
-            "messagingSenderId": "1012794337509"
-        })), provideFirestore(() => getFirestore())]
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({
+        "projectId": "bjjtournament-4538b",
+        "appId": "1:1012794337509:web:8cf044ab0853b812460c0c",
+        "storageBucket": "bjjtournament-4538b.firebasestorage.app",
+        "apiKey": "AIzaSyBLTBSPgxF_PWyzQpIOGb5Uu9Ggs-IO3Ss",
+        "authDomain": "bjjtournament-4538b.firebaseapp.com",
+        "messagingSenderId": "1012794337509"
+    })), provideFirestore(() => getFirestore()),
+    provideStore()
+]
 };
