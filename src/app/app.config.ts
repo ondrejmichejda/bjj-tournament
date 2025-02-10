fe3a44bebc8e99162660c2815954cb757b2d7bca
Ondřej Michejda <ondra.michejda@gmail.com>
2025-02-10 11:09:21 +0100

    firestore init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+    providers: [
+        provideZoneChangeDetection({eventCoalescing: true}),
+        provideRouter(routes),
+        provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({
+            "projectId": "bjjtournament-4538b",
+            "appId": "1:1012794337509:web:8cf044ab0853b812460c0c",
+            "storageBucket": "bjjtournament-4538b.firebasestorage.app",
+            "apiKey": "AIzaSyBLTBSPgxF_PWyzQpIOGb5Uu9Ggs-IO3Ss",
+            "authDomain": "bjjtournament-4538b.firebaseapp.com",
+            "messagingSenderId": "1012794337509"
+        })), provideFirestore(() => getFirestore())]
 };
